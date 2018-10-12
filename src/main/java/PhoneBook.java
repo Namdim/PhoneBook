52faeec1316083d42a28b5a754d23436e4319632
Anatolii Nizkopoklonnyi <a.nizkopoklonnyi@gmail.com>
2018-10-12 09:07:56 +0300

    Add [PhoneBook],method printContact

---
+++ src/main/java/PhoneBook.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PhoneBook {
+    private HashMap<String, ArrayList<String>> contacts ;
+    private ArrayList<ArrayList<String>> phoneNumbs;
+
+    public PhoneBook(){
+        contacts = new HashMap<String, ArrayList<String>>();
+        phoneNumbs = new ArrayList<ArrayList<String>>();
+
+        addStartDataPhoneBook();
+    }
+    public void printContact(String fio){
+        System.out.print( contacts.get(fio) );
+        System.out.println();
+    }
+
+    private void addStartDataPhoneBook(){
+        phoneNumbs.add(new ArrayList<String>());
+        phoneNumbs.get(0).add("+8 800 2000 500");
+        phoneNumbs.get(0).add("+8 800 2000 600");
+
+        phoneNumbs.add(new ArrayList<String>());
+        phoneNumbs.get(1).add("+8 800 2000 700");
+
+        phoneNumbs.add(new ArrayList<String>());
+        phoneNumbs.get(2).add("+8 800 2000 800");
+        phoneNumbs.get(2).add("+8 800 2000 900");
+        phoneNumbs.get(2).add("+8 800 2000 000");
+
+        contacts.put("Иванов И.И.", phoneNumbs.get(0));
+        contacts.put("Петров П.П.", phoneNumbs.get(1));
+        contacts.put("Сидоров С.С.", phoneNumbs.get(2));
+    }
+
+
+}
--- src/main/java/TestTask2.java
+++ src/main/java/TestTask2.java
@@ -3,25 +3,17 @@ import java.util.HashMap;
 
 public class TestTask2 {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> phoneNumbs = new ArrayList<ArrayList<String>>();
+    String fioSidorov = "Сидоров С.С.";
+    String fioIvanov = "Иванов И.И.";
+    String fioPetrov = "Петров П.П.";
 
-        phoneNumbs.add(new ArrayList<String>());
-        phoneNumbs.get(0).add("+8 800 2000 500");
-        phoneNumbs.get(0).add("+8 800 2000 600");
+    PhoneBook phoneBook = new PhoneBook();
+    phoneBook.printContact(fioSidorov);
+    phoneBook.printContact(fioIvanov);
+    phoneBook.printContact(fioPetrov);
 
-        phoneNumbs.add(new ArrayList<String>());
-        phoneNumbs.get(1).add("+8 800 2000 700");
 
-        phoneNumbs.add(new ArrayList<String>());
-        phoneNumbs.get(2).add("+8 800 2000 800");
-        phoneNumbs.get(2).add("+8 800 2000 900");
-        phoneNumbs.get(2).add("+8 800 2000 000");
 
-        HashMap<String, ArrayList<String>> phoneBook = new HashMap<String, ArrayList<String>>();
-        phoneBook.put("Иванов И.И.", phoneNumbs.get(0));
-        phoneBook.put("Петров П.П.", phoneNumbs.get(1));
-        phoneBook.put("Сидоров С.С.", phoneNumbs.get(2));
 
-        System.out.print( phoneBook.get("Сидоров С.С.") );
     }
 }
